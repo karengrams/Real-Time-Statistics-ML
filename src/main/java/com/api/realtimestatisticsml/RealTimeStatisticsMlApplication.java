af65bb11e73deddfac8d8600a17f45c08e0a3fba
Karen Manrique <32707822+karengrams@users.noreply.github.com>
2021-04-17 12:12:05 -0300

    :heavy_plus_sign: :tada: Creating skeleton and adding Spring Boot depedency

---
+++ src/main/java/com/api/realtimestatisticsml/RealTimeStatisticsMlApplication.java
@@ -0,0 +1,13 @@
+package com.api.realtimestatisticsml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeStatisticsMlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealTimeStatisticsMlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/api/realtimestatisticsml/RealTimeStatisticsMlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.realtimestatisticsml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeStatisticsMlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
