928da328d608ef95c4d63a82ae47e2d8f09dc4b7
Karen Manrique <32707822+karengrams@users.noreply.github.com>
2021-04-18 00:58:45 -0300

    :sparkles: Finishing transactions controller

--- src/main/java/com/api/realtimestatisticsml/controllers/TransactionController.java
+++ src/main/java/com/api/realtimestatisticsml/controllers/TransactionController.java
@@ -1,36 +1,38 @@
 package com.api.realtimestatisticsml.controllers;
 
+import com.api.realtimestatisticsml.cache.TransactionsCache;
 import com.api.realtimestatisticsml.models.Transaction;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.OffsetDateTime;
-import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class TransactionController {
 
-    private ArrayList<Transaction> transactions = new ArrayList<Transaction>();
+    private TransactionsCache transactionsCache;
 
-    public void addNewTransaction(Transaction transaction){
-        this.transactions.add(transaction);
+    public TransactionController(@Autowired TransactionsCache transactionsCache) {
+        this.transactionsCache = transactionsCache;
     }
 
     @GetMapping("/transactions")
-    public ArrayList<Transaction> getTransactions(){
-        return this.transactions;
+    public List<Transaction> getTransactions() {
+        return this.transactionsCache.getAllTransactions();
     }
 
     @PostMapping("/transactions")
-    public ResponseEntity newTransaction(@RequestParam(value="timestamp") String timestamp, @RequestParam(value="amount") String amount){
-        this.addNewTransaction(new Transaction(OffsetDateTime.parse(timestamp), Float.parseFloat(amount)));
-        return ResponseEntity.ok(HttpStatus.OK);
+    public ResponseEntity<Void> newTransaction(@RequestParam(value = "timestamp") String timestamp, @RequestParam(value = "amount") String amount) {
+        this.transactionsCache.addNewTransaction(new Transaction(OffsetDateTime.parse(timestamp), Float.parseFloat(amount)));
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @DeleteMapping("/transactions")
-    public void deleteTransactions(){
-        this.transactions= new ArrayList<Transaction>();
+    public void deleteTransactions() {
+        this.transactionsCache.cleanCache();
     }
 }
