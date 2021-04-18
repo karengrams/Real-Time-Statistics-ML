541092a2da1c2ec0b34d20f4ebbd5cb26aee72c0
Karen Manrique <32707822+karengrams@users.noreply.github.com>
2021-04-18 15:20:03 -0300

    :construction: Handling unaccepted cases

--- src/main/java/com/api/realtimestatisticsml/controllers/TransactionController.java
+++ src/main/java/com/api/realtimestatisticsml/controllers/TransactionController.java
@@ -8,8 +8,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.Time;
+import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
+import java.time.temporal.TemporalUnit;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 public class TransactionController {
@@ -27,12 +31,31 @@ public class TransactionController {
 
     @PostMapping("/transactions")
     public ResponseEntity<Void> newTransaction(@RequestParam(value = "timestamp") String timestamp, @RequestParam(value = "amount") String amount) {
-        this.transactionsCache.addNewTransaction(new Transaction(OffsetDateTime.parse(timestamp), Float.parseFloat(amount)));
+        OffsetDateTime transactionTimestamp = null;
+        Float transactionAmount = null;
+        try {
+            transactionTimestamp = OffsetDateTime.parse(timestamp);
+            transactionAmount = Float.parseFloat(amount);
+        }
+        catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.UNPROCESSABLE_ENTITY); // TODO: Handlear mis propias exceptions
+        }
+
+        if(transactionTimestamp.isAfter(OffsetDateTime.now())){
+            return new ResponseEntity<>(HttpStatus.UNPROCESSABLE_ENTITY); // TODO: Handlear mis propias exceptions
+        }
+
+        if(transactionTimestamp.isBefore(OffsetDateTime.now().minusSeconds(60))){
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT); // TODO: Handlear mis propias exceptions
+        }
+
+        this.transactionsCache.addNewTransaction(new Transaction(transactionTimestamp,transactionAmount));
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @DeleteMapping("/transactions")
-    public void deleteTransactions() {
+    public ResponseEntity<Void> deleteTransactions() {
         this.transactionsCache.cleanCache();
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
