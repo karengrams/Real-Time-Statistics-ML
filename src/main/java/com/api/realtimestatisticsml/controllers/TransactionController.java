40f20ff5e243dc973421c0e6b2e78cb5f5983e95
Karen Manrique <32707822+karengrams@users.noreply.github.com>
2021-04-18 23:27:37 -0300

    :recycle: Creating Transaction Service

--- src/main/java/com/api/realtimestatisticsml/controllers/TransactionController.java
+++ src/main/java/com/api/realtimestatisticsml/controllers/TransactionController.java
@@ -1,61 +1,35 @@
 package com.api.realtimestatisticsml.controllers;
 
-import com.api.realtimestatisticsml.cache.TransactionsCache;
 import com.api.realtimestatisticsml.models.Transaction;
 
+import com.api.realtimestatisticsml.services.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.sql.Time;
-import java.time.LocalDateTime;
-import java.time.OffsetDateTime;
-import java.time.temporal.TemporalUnit;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 @RestController
 public class TransactionController {
 
-    private TransactionsCache transactionsCache;
+    private TransactionService transactionService;
 
-    public TransactionController(@Autowired TransactionsCache transactionsCache) {
-        this.transactionsCache = transactionsCache;
+    public TransactionController(@Autowired TransactionService transactionService) {
+        this.transactionService = transactionService;
     }
 
     @GetMapping("/transactions")
     public List<Transaction> getTransactions() {
-        return this.transactionsCache.getAllTransactions();
+        return this.transactionService.getTransactions();
     }
 
     @PostMapping("/transactions")
     public ResponseEntity<Void> newTransaction(@RequestParam(value = "timestamp") String timestamp, @RequestParam(value = "amount") String amount) {
-        OffsetDateTime transactionTimestamp = null;
-        Float transactionAmount = null;
-        try {
-            transactionTimestamp = OffsetDateTime.parse(timestamp);
-            transactionAmount = Float.parseFloat(amount);
-        }
-        catch (Exception e){
-            return new ResponseEntity<>(HttpStatus.UNPROCESSABLE_ENTITY); // TODO: Handlear mis propias exceptions
-        }
-
-        if(transactionTimestamp.isAfter(OffsetDateTime.now())){
-            return new ResponseEntity<>(HttpStatus.UNPROCESSABLE_ENTITY); // TODO: Handlear mis propias exceptions
-        }
-
-        if(transactionTimestamp.isBefore(OffsetDateTime.now().minusSeconds(60))){
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT); // TODO: Handlear mis propias exceptions
-        }
-
-        this.transactionsCache.addNewTransaction(new Transaction(transactionTimestamp,transactionAmount));
-        return new ResponseEntity<>(HttpStatus.CREATED);
+        return this.transactionService.postTransaction(timestamp, amount);
     }
 
     @DeleteMapping("/transactions")
     public ResponseEntity<Void> deleteTransactions() {
-        this.transactionsCache.cleanCache();
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        return this.transactionService.cleanCache();
     }
 }
---
+++ src/main/java/com/api/realtimestatisticsml/services/TransactionService.java
@@ -0,0 +1,54 @@
+package com.api.realtimestatisticsml.services;
+
+import com.api.realtimestatisticsml.models.Transaction;
+import com.api.realtimestatisticsml.repository.TransactionsCache;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.time.*;
+import java.util.List;
+
+@Service
+public class TransactionService {
+
+    private TransactionsCache transactionsCache;
+
+    public TransactionService(@Autowired TransactionsCache transactionsCache) {
+        this.transactionsCache = transactionsCache;
+    }
+
+    public ResponseEntity<Void> postTransaction(String timestamp, String amount) {
+        Instant transactionTimestamp = null;
+        Float transactionAmount = null;
+
+        try {
+            transactionTimestamp = Instant.parse(timestamp);
+            transactionAmount = Float.parseFloat(amount);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.UNPROCESSABLE_ENTITY);
+        }
+
+        if (Instant.now().isBefore(transactionTimestamp)) {
+            return new ResponseEntity<>(HttpStatus.UNPROCESSABLE_ENTITY);
+        }
+
+        if (transactionTimestamp.isBefore(Instant.now().minusSeconds(60))) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+
+        this.transactionsCache.addNewTransaction(new Transaction(transactionTimestamp, transactionAmount));
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    public List<Transaction> getTransactions() {
+        return this.transactionsCache.getAllTransactions();
+    }
+
+    public ResponseEntity<Void> cleanCache() {
+        this.transactionsCache.cleanCache();
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+}
